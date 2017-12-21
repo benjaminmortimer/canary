ea01436c3b4fa6aefd5a71d2077030e307df46f6
Benjamin Mortimer <benjamin.mortimer@live.com>
2017-12-21 02:36:49 +0000

    Add graphs

--- canary.rb
+++ canary.rb
@@ -1,4 +1,5 @@
 require './lib/report.rb'
+require './lib/graphify.rb'
 require 'sinatra'
 
 set :port, 8080
@@ -7,17 +8,14 @@ set :public_folder, "static"
 set :views, "views"
 
 get '/' do
-	erb :index, :locals => {:stats => ""}
+	erb :index, :locals => {:stats => "", :graph =>""}
 end
 
 post '/info' do
 	slug = params['slug']
 	url = "https://www.gov.uk/api/content#{slug}"
 	erb :info, :locals => {:text_of_slug => slug,
-		:stats => Report::report(url)}
+		:stats => Report::report(url),
+		:graph => Graphify::graph(600, 400, Graphify::get_data('./data/canary_data.csv'),Report::report(url))
+	}
 end
-
-
-
-
-
